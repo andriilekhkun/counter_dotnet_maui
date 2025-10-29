5d906d56c03235749b9ba7c6977090996957458b
Lekhkun <andrijlehkun1@gmail.com>
2025-10-29 20:40:00 +0100

    Added files 'MainPage.xaml' and 'MainPage.xaml.cs' to repository

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Counter_Lekhkun_Andrii.Controls;
+using Counter_Lekhkun_Andrii.Views;
+using System.Windows.Input;
+
+namespace Counter_Lekhkun_Andrii
+{
+    public partial class MainPage : ContentPage
+    {
+        public ICommand IncreaseCommand { get; set; }
+        public ICommand DecreaseCommand { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent(); 
+            
+            IncreaseCommand = new RelayCommand(() => myCounter.CounterCount++);
+            DecreaseCommand = new RelayCommand(() => myCounter.CounterCount--);
+
+            BindingContext = this;
+        }
+    }
+}
